e8ab932fb10290c7d64c1d0ea276d9ac49b4462a
KulturExplora <developer@kulturexplora.com>
2025-02-16 22:45:10 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   //base: process.env.NODE_ENV === 'production' ? '/KulturExplora/' : '/',
-
+  base: '/', // Use '/' for root deployment
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
 });
