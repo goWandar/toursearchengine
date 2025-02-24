209e0064f4a70d044fdb9594d5183e3e585ccaea
KulturExplora <developer@kulturexplora.com>
2025-02-24 23:14:45 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,14 +11,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(
-    cors({
-        origin: [
-            `http://localhost:${port}`,
-            "https://kulturexploratest.onrender.com",
-        ],
-    })
-);
+app.use(cors({
+  origin: 'https://kulturexploratest.netlify.app'
+}));
 
 
 app.use(express.json());
