054e3924d1382dcc51423593fda952604a945123
KulturExplora <developer@kulturexplora.com>
2025-02-24 23:09:00 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,33 +11,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-//app.use(
-//    cors({
-//        origin: [
-//            `http://localhost:${port}`,
-//            "https://kulturexploratest.onrender.com",
-//        ],
-//    })
-//);
+app.use(
+    cors({
+        origin: [
+            `http://localhost:${port}`,
+            "https://kulturexploratest.onrender.com",
+        ],
+    })
+);
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://kulturexploratest.onrender.com"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-
-next();
-});
 
 app.use(express.json());
 
