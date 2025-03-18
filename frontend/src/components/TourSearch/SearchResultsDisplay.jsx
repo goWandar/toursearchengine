940fc20a778086c67409e15c837b50a761e2ddea
arun pariyar <arun.pariyar@gmail.com>
2025-03-18 21:50:40 +0100

    🚧 Fix connection to api
    There is still lots of work to do here because consideration needs to be take for the images which can also be empty. currently only the location filter is the thing that works

--- frontend/src/components/TourSearch/SearchResultsDisplay.jsx
+++ frontend/src/components/TourSearch/SearchResultsDisplay.jsx
@@ -1,26 +1,32 @@
-import PropTypes from 'prop-types';
-import TourCard from './TourCard';
-import NoResults from './NoResults';
-import '../../assets/_searchResultsDisplay.scss';
+import PropTypes from "prop-types";
+import TourCard from "./TourCard";
+import NoResults from "./NoResults";
+import "../../assets/_searchResultsDisplay.scss";
 
 const SearchResultsDisplay = ({ results, onBookNow, hasSearched }) => {
+  console.log("results", results);
   return (
-    <div className={`search-results ${results.length === 0 && hasSearched ? 'no-results-center' : ''}`}>
+    <div
+      className={`search-results ${
+        results.length === 0 && hasSearched ? "no-results-center" : ""
+      }`}
+    >
       {hasSearched &&
         (results.length > 0 ? (
-          results.map(tour => (
+          results.map((tour) => (
             <TourCard
               key={tour.id}
-              image={tour.image}
+              //FIXME this is a hack to get the first image
+              image={tour.images[0].image_urls}
               title={tour.title}
               price={tour.price}
               country={tour.country}
-              places={tour.places}
+              places={tour.location.split(",")}
               onBookNow={() => onBookNow(tour.id)}
             />
           ))
         ) : (
-          <NoResults message='No tours match your search criteria. Please try again.' />
+          <NoResults message="No tours match your search criteria. Please try again." />
         ))}
     </div>
   );
@@ -35,7 +41,7 @@ SearchResultsDisplay.propTypes = {
       price: PropTypes.number.isRequired,
       country: PropTypes.string.isRequired,
       places: PropTypes.arrayOf(PropTypes.string).isRequired,
-    }),
+    })
   ).isRequired,
   onBookNow: PropTypes.func.isRequired,
   hasSearched: PropTypes.bool.isRequired,
