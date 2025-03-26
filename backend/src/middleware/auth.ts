3b4f288ec4dd8e600caabd08325f98dd71f45137
jacekroszkowiak <jacekroszkowiakdev@gmail.com>
2025-03-26 13:53:18 +0100

    feat(auth): test SupaBase JWT

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -3,18 +3,26 @@ import jwt from "jsonwebtoken";
 
 const supabaseJwtSecret = process.env.SUPABASE_JWT_SECRET as string;
 
-export function authToken(req: Request, res: Response, next: NextFunction) {
+export function authenticateToken(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1]; // Bearer <token>
 
-    if (!token)
-        return res
-            .status(401)
-            .json({ error: "Access denied: no token provide" });
+    if (!token) {
+        res.status(401).json({ error: "Access denied: no token provide" });
+        return;
+    }
 
-    jwt.verify(token, supabaseJwtSecret, (err, user) => {
-        if (err) return res.status(403).json({ error: "Invalid token" });
-        req.user = user;
-        next();
-    });
+    try {
+        jwt.verify(token, supabaseJwtSecret, (err, user) => {
+            if (err) return res.status(403).json({ error: "Invalid token" });
+            req.user = user;
+            next();
+        });
+    } catch (err) {
+        res.status(403).json({ error: "Invalid token" });
+    }
 }
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,14 +1,28 @@
 import { Request, Response, Router } from "express";
 import { UserService } from "../services/user.service";
 import { responseHandler } from "../utils/responseHandler";
+import { authenticateToken } from "../middleware/auth";
+
 const router = Router();
 
 // POST Create User
 router.post("/user", async (req: Request, res: Response) => {
-  const { name, email } = req.body;
-  const result = await UserService.createUser(name, email);
+    const { name, email } = req.body;
+    const result = await UserService.createUser(name, email);
 
-  responseHandler(res, result, "POST");
+    responseHandler(res, result, "POST");
 });
+
+router.get(
+    "/user-auth-test",
+    authenticateToken,
+    async (req: Request, res: Response) => {
+        const user = req.user;
+        res.json({
+            message: "Authenticated route hit successfully!",
+            user,
+        });
+    }
+);
 
 export default router;
