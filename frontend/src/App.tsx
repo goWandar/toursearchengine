7f9fa5ea909457f87940e78e53e197a0c4b68d8f
Ayo <aysamuel007@gmail.com>
2025-06-21 20:46:50 +0200

    feat(router): implement routing for TextSearchPage and update layout structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,33 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Hero from './components/layout/Hero';
 import HowWandarWorksLayout from './components/layout/How';
+import TextSearchPage from './components/TextSearch/TextSearchPage';
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Hero />
-        <div className='max-w-[2500px] mx-auto'>
-          <HowWandarWorksLayout />
-        </div>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <>
+              <Hero />
+              <div className='max-w-[2500px] mx-auto'>
+                <HowWandarWorksLayout />
+              </div>
+            </>
+          }
+        />
+        <Route
+          path='/search'
+          element={
+            <>
+              <TextSearchPage />
+            </>
+          }
+        />
+      </Routes>
+    </Router>
   );
 };
 
