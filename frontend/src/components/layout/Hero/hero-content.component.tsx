dbff24216be72599c36ea4bdad58716d94cd7140
Henry Nicholas Chettiar (KAAR INFOTECH PRIVATE LIMITED) <v-henryn@microsoft.com>
2025-07-02 22:06:20 +0530

    landing page- content changes

--- frontend/src/components/layout/Hero/hero-content.component.tsx
+++ frontend/src/components/layout/Hero/hero-content.component.tsx
@@ -6,24 +6,24 @@ const HeroContent = () => {
     <div className='flex flex-col items-center gap-20'>
       
       {/* WE ARE WANDAR section */}
-      <div className='flex items-center gap-2'>
+      {/* <div className='flex items-center gap-2'>
         <div className='w-[1.62894rem] h-[0.0665rem] bg-[#F4A261]' />
         <span className='text-[#F4A261] text-base font-light capitalize'>WE ARE WANDAR</span>
         <div className='w-[1.62894rem] h-[0.0665rem] bg-[#F4A261]' />
-      </div>
+      </div> */}
 
       {/* Main heading */}
       <div className='flex flex-col items-center'>
         <h1 className='text-white text-[7rem] font-semibold capitalize leading-none text-center'>
-          YOUR SAFARI
+          Find the safari that fits without the stress
         </h1>
         <h1 className='text-[#F4A261] text-[7rem] font-semibold capitalize leading-none text-center'>
-          BEGINS HERE
+          Most safari sites give you options - Wandar helps you choose
         </h1>
       </div>
 
       {/* Buttons */}
-      <div className='flex gap-6'>
+      {/* <div className='flex gap-6'>
         <Button
           id='discover-more-button'
           label='Discover more'
@@ -39,7 +39,7 @@ const HeroContent = () => {
           hasBorder={true}
           className='text-white hover:text-black'
         />
-      </div>
+      </div> */}
     </div>
   );
 };
